e7131b38aaf4d6e711f390f008eb909d8ea48666
maestroTW <nayan10122007@gmail.com>
2025-04-09 23:16:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import time
 from datetime import datetime
-from pathlib import Path
+# from pathlib import Path
+# import os
 
 log_file_path = r"C:\Users\user\AppData\LocalLow\Northwood\SCPSL\Player.log"
-
+session_path = r"C:\Users\user\AppData\LocalLow\Northwood\SCPSL\session.log"
 def follow_log_file():
-    with open(log_file_path, 'r', encoding='utf-8') as log_file:
+    with open(log_file_path, 'r', encoding='utf-8') as log_file, \
+        open(session_path, 'a', encoding='utf-8') as session_log:
 
         for line in log_file:
             print(line.strip())
-
-
+            session_log.write(line.strip())
         log_file.seek(0, 2)
 
-
         print("[INFO] START RECORDING")
-
+        session_log.write("[INFO] START RECORDING\n")
         while True:
             line = log_file.readline()
 
@@ -23,17 +23,25 @@ def follow_log_file():
                 time.sleep(0.1)
                 continue
 
-            # delet spaces
             line = line.strip()
-
             current_time = datetime.now()
 
-
             print(f"[{current_time}] {line}")
+            session_log.write(f"[{current_time}] {line}\n")
 
-            #if line == "--End of Log--":
-
-
+            # stop_recording = False
+            # if not line and stop_recording:
+            #     print("FINISHED RECORDING")
+            #     session_log.write("FINISHED RECORDING\n")
+            #     session_log.close()
+            #     os.rename(session_path, f"session{current_time.strftime('%Y%m%d%H')}")
+            #     break
+            #
+            # if line == "--End of Log--":
+            #     print("[INFO] STOPPING")
+            #     session_log.write("[INFO] STOPPING\n")
+            #     stop_recording = True
+            #     time.sleep(5)
 
 if __name__ == "__main__":
     try:
